9e542eb8113f0ffb1dca751958e1e007a55a79d5
Jim Crowell <jamacrow@iu.edu>
2026-06-29 15:59:08 -0400

    think I've got the 'close float_diff' keymap working

--- lua/atone/core.lua
+++ lua/atone/core.lua
@@ -289,7 +289,9 @@ local mappings = {
                     return
                 end
             end
-            utils.keymap("n", "gd", closeit, {
+            -- Steal float_diff keymap from tree view to close it
+            local lhs = keymaps_conf.tree.float_diff
+            utils.keymap("n", lhs, closeit, {
                 desc = "Close floating diff",
                 buffer = M._centered_diff_buf
             })
